af0d18a3f13cda3ca479fd817121a4f09a20bef4
jewong42 <jwong42@outlook.com>
2019-06-18 14:08:32 -0700

    Refactored method of loading modal data. Modals now load images.

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,28 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('./database');
-
-var projects, education;
-var skills_pw, skills_ew, skills_ww;
+var fs = require('fs');
 
 router.get('/', async function (req, res, next) {
   try {
-    projects = await pool.query('SELECT * FROM projects')
-    education = await pool.query('SELECT * FROM education')
-    skills_pw = await pool.query('SELECT * FROM skills WHERE level = "proficient with"')
-    skills_ew = await pool.query('SELECT * FROM skills WHERE level = "experienced with"')
-    skills_ww = await pool.query('SELECT * FROM skills WHERE level = "worked with"')
+    var projects = await pool.query('SELECT * FROM projects')
+    getProjectImages(projects);
+    var education = await pool.query('SELECT * FROM education')
+    var skills_pw = await pool.query('SELECT * FROM skills WHERE level = "proficient with"')
+    var skills_ew = await pool.query('SELECT * FROM skills WHERE level = "experienced with"')
+    var skills_ww = await pool.query('SELECT * FROM skills WHERE level = "worked with"')
   } catch (err) {
     throw new Error(err)
   }
-  console.log(projects);
-  console.log(skills_pw);
-  res.render('index', { projects: projects, education: education, skills_pw: skills_pw, skills_ew: skills_ew, skills_ww: skills_ww });
+  res.render('index', { projects: projects, education: education, skills_pw: skills_pw, skills_ew: skills_ew, skills_ww: skills_ww, images: ["test1"] });
 });
+
+function getProjectImages(projects) {
+  var files;
+  for (index in projects) {
+    files = fs.readdirSync("./public/" + projects[index].image_source + "/");
+    projects[index].images = files;
+  }
+}
 
 module.exports = router;
